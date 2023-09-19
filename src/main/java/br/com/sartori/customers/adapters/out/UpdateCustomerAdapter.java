1f4d8789a7d1e1d1a0046cda63b6257a608f08ab
Rodrigo Sartori <93sartori@gmail.com>
2023-09-19 20:52:22 -0300

    Criado adapter para update Customer

---
+++ src/main/java/br/com/sartori/customers/adapters/out/UpdateCustomerAdapter.java
@@ -0,0 +1,21 @@
+package br.com.sartori.customers.adapters.out;
+
+import br.com.sartori.customers.adapters.out.database.mapper.CustomerEntityMapper;
+import br.com.sartori.customers.adapters.out.database.repository.CustomerRepository;
+import br.com.sartori.customers.application.core.domain.Customer;
+import br.com.sartori.customers.application.ports.out.UpdateCustomerOutputPort;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UpdateCustomerAdapter implements UpdateCustomerOutputPort {
+
+    @Autowired
+    private CustomerRepository customerRepository;
+
+    @Override
+    public void update(Customer customer) {
+        var customerEntity = CustomerEntityMapper.INSTANCE.toCustomerEntity(customer);
+        customerRepository.save(customerEntity);
+    }
+}
